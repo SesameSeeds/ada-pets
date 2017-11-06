d7af7942ef80117ebb0b29c3b935483944aad9b5
Stef <seedsofthesesame@gmail.com>
2017-11-06 10:31:23 -0800

    updated controller and routes.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,4 +6,27 @@ class PetsController < ApplicationController
       json: pets.as_json(only: [:age, :id, :human, :name]),status: :ok
     )
   end
+
+  def show
+
+    pet = Pet.find_by(id: params[:id])
+
+    if pet
+      render json: pet.as_json(only: [:age, :human, :id, :name]), status: :ok
+    else
+      render json: { ok: false }, status: :not_found
+    end
+
+  end
+
+  def create
+    pet = Pet.new(pet_params)
+    pet.save!
+    render json: { id: pet.id }, status: :ok
+  end
+
+  private
+  def pet_params
+    params.require(:pet).permit(:name, :age, :human)
+  end
 end
